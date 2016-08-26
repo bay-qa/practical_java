7cdc64aaedf9c727c2a68682b4670a8e8326a5bd
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2016-08-26 16:09:08 -0700

    adding Base super class

--- src/test/java/pageobjects/Login.java
+++ src/test/java/pageobjects/Login.java
@@ -3,11 +3,15 @@ package pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by idorovskikh on 8/25/16.
  */
-public class Login {
+public class Login extends Base {
     private WebDriver driver;
+
+    By loginFormLocator = By.id("login");
 
     By usernameLocator  = By.id("username");
     By passwordLocator  = By.id("password");
@@ -15,18 +19,21 @@ public class Login {
     By successMessageLocator = By.cssSelector(".flash.success");
 
     public Login(WebDriver driver){
-        this.driver = driver;
-        driver.get("http://the-internet.herokuapp.com/login");
+        super(driver);
+        visit("http://the-internet.herokuapp.com/login");
+        assertTrue("The login form is not present",
+                isDisplayed(loginFormLocator));
     }
 
     public void with (String username, String password){
-        driver.findElement(usernameLocator).sendKeys(username);
-        driver.findElement(passwordLocator).sendKeys(password);
-        driver.findElement(submitButton).click();
+        type(username, usernameLocator);
+        type(password, passwordLocator);
+
+        click(submitButton);
     }
 
     public Boolean successMessagePresent(){
-        return driver.findElement(successMessageLocator).isDisplayed();
+        return isDisplayed(successMessageLocator);
     }
 
 }
